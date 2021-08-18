7b391a05b1e26e254075c917d3a1f0d2929e993f
Chananan Likitsarawit <chananan.anu@mail.kmutt.ac.th>
2021-08-19 01:29:27 +0700

    Home

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Team from '../views/Team.vue'
+import Basket from '../views/Basket.vue'
 
 const routes = [
   {
@@ -14,7 +17,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component: Team
+  },
+  {
+    path: '/basket',
+    name: 'Basket',
+    component: Basket
+  },
 ]
 
 const router = createRouter({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: '#F4F4F4',
+        gray: '#A3A3A3',
+        primary: '#2FDBBC',
+        dark: '#2D2D2D'
+      }
+    },
   },
   variants: {
+    fill: ['hover', 'focus'],
     extend: {},
   },
   plugins: [],
